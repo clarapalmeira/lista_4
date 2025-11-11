c2fc694b660ec214516befdfe5ece5b148a83226
clarapalmeira <clara.palmeira12@aluno.ifce.edu.br>
2025-11-11 16:04:30 -0300

    Create q2.c

---
+++ q2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, soma;
+    int *p1, *p2, *pSoma;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &a);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &b);
+
+  
+    p1 = &a;
+    p2 = &b;
+    pSoma = &soma;
+
+    *pSoma = *p1 + *p2;  
+
+    printf("\nSoma: %d\n", *pSoma);
+    printf("Endereco da soma: %p\n", (void*)pSoma);
+
+    return 0;
+}
